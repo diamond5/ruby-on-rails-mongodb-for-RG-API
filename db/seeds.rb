865661bb7253ce8a0b57d3ca586c102385019215
diamond5 <masterofworld20000@gmail.com>
2017-09-01 18:10:03 -0400

    add db configurations with seed

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create! email: 'test_user1@test.com', first_name: 'Test', last_name: 'User'
+User.create! email: 'test_user2@test.com', first_name: 'Test', last_name: 'User'
+User.create! email: 'test_user3@test.com', first_name: 'Test', last_name: 'User'
