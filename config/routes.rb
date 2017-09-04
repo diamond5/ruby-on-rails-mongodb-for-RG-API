5dbb4e3d5fe24283de1bb671a35f192ed9c4c47a
diamond5 <masterofworld20000@gmail.com>
2017-09-04 18:26:12 -0400

    add devise for authentication and add login and signup apis, add authenticate user on users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
-      resources :users
+      resources :users do
+        collection do
+          post 'login'
+          post 'signup'
+        end
+      end
     end
   end
 end
